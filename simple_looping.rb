8af9f1959ebcd6b0d1cbe81cecfbd18943cbeb88
Devon Darrow <ddarrowphl@gmail.com>
2018-08-13 20:02:53 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -14,8 +14,9 @@ def loop_iterator(number_of_times)
 end
 
 def times_iterator(number_of_times)
+  7.times do
   # code your solution here using the "times" keyword
-  phrase = "Welcome to Flatiron School's Web Development Course!"
+  puts "Welcome to Flatiron School's Web Development Course!"
 
 end
 
