48a8104047557dca351f034d66a62adeb1adda85
Devon Darrow <ddarrowphl@gmail.com>
2018-08-13 22:19:10 +0000

    automatically backed up by learn

--- spec/simple_looping_spec.rb
+++ spec/simple_looping_spec.rb
@@ -5,7 +5,7 @@ describe 'Loops' do
     @looping_string = "Welcome to Flatiron School's Web Development Course!\nWelcome to Flatiron School's Web Development Course!\nWelcome to Flatiron School's Web Development Course!\nWelcome to Flatiron School's Web Development Course!\nWelcome to Flatiron School's Web Development Course!\nWelcome to Flatiron School's Web Development Course!\nWelcome to Flatiron School's Web Development Course!\n"
   end
 
-  describe 'loop', :focus => true  do
+  describe 'loop' do
     it "prints out Welcome to Flatiron School\'s Web Development Course! 7 times" do
       expect{ loop_iterator(7) }.to output(@looping_string).to_stdout
     end
