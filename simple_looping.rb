0c0a60f481bf903232259b603cf9e865445182aa
Devon Darrow <ddarrowphl@gmail.com>
2018-08-13 20:15:12 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -28,12 +28,12 @@ def times_iterator(number_of_times)
 end
 
 def while_iterator(number_of_times)
-  #number_of_times = 0
+  number_of_times = 0
 
   while number_of_times < 7
-
+    number_of_times += 1
   puts "Welcome to Flatiron School's Web Development Course!"
-  number_of_times += 1
+
   end
 
 end
