868befcb3c4e988b450fdadb7e47e82f4a5cb0fe
Devon Darrow <ddarrowphl@gmail.com>
2018-08-13 22:20:25 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,7 +1,7 @@
 # REMEMBER: print your output to the terminal using 'puts'
 
 def loop_iterator(number_of_times)
-  number_of_times = 0
+  counter = 0
 
   loop do
 
@@ -12,7 +12,7 @@ def loop_iterator(number_of_times)
   #   correct number of times?
   # Maybe we should keep count of the number of times we've puts out the
   #  phrase and break when the counter hits the appropriate number...
-  number_of_times += 1
+  counter += 1
   puts "Welcome to Flatiron School's Web Development Course!"
   if counter >= 7
     break
